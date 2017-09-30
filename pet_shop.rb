fb3cc21963df303a5457c9f4ae3cb98734542d1d
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-09-30 12:42:27 +0100

    passed both customer_can_afford_pet tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -63,3 +63,10 @@ end
 def add_pet_to_customer(customers, new_pet)
   return customers[:pets] << new_pet
 end
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] >= new_pet[:price]
+    return true
+  end
+  return false
+end
